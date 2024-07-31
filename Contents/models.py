c453eb21db70be63ba876cfc808481c0c4ff01d3
Wahidul Islam <wahidulislambd4@gmail.com>
2024-07-31 23:15:02 +0600

    user start

--- Contents/models.py
+++ Contents/models.py
@@ -4,14 +4,15 @@ from authentication.models import BaseModel, CustomUser
 class Photo(BaseModel):
     url = models.ImageField(upload_to='post_pictures/',blank=True, height_field=None, width_field=None, max_length=None)
  
+
+REACTION_OPTIONS = [('likes','Likes'),('love','Love'),('sad', 'Sad'),('wow', 'Wow')]
+
 class Post(BaseModel):
-    
     user = models.ForeignKey(CustomUser, related_name="user", on_delete=models.CASCADE)
     text = models.TextField()
     image = models.ForeignKey(Photo, blank=True)
     video = models.FileField(upload_to='post_video/', max_length=100)
-    likes = models.CharField(max_length=10,choices=[('likes','Likes'),('love','Love'),('sad', 'Sad'),('wow', 'Wow')])
-
+    likes = models.CharField(max_length=10, choices=REACTION_OPTIONS)
 
 class Comment(BaseModel):
     post = models.ForeignKey(Post, related_name="post", on_delete=models.CASCADE)
